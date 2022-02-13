8f9e718923104ab28251e35956463d5607d25528
Sabarna Ghosh Dastidar <73946046+gdsabarna353@users.noreply.github.com>
2022-02-13 23:11:41 +0530

    file added

---
+++ style.js
@@ -0,0 +1,19 @@
+function compute()
+{
+   var Amount = document.getElementById("principal");
+   var Rate = document.getElementById("rate");
+   var Years = document.getElementById("years");
+   if(Amount.value<=0){
+       alert("Enter a positive number");
+       return false;
+   }
+   if(Rate.value==0){
+    alert("please enter a positive value of Rate");
+    return false;
+}
+
+   var final = parseFloat(Amount.value) *(1 + parseFloat(Rate.value) * parseInt(Years.value) /100);
+  final = final.toFixed(2);
+   Years = 2021+parseInt(Years.value);
+   document.getElementById("result").innerHTML="If you deposit today Rs."+"<mark>"+Amount.value+"</mark>"+"<br>"+"at an interest rate of "+"<mark>"+Rate.value+"%."+"</mark>"+"<br>"+"You will receive an amount of Rs."+"<mark>"+final+"</mark>"+","+"<br>"+"in the year "+"<mark>"+Years+"</mark>"+"."
+}
